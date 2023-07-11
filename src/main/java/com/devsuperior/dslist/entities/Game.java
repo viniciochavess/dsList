6263ce37ce7e486a38b3ec8a90467bd3a72d79d0
viniciochavess <47008512+viniciochavess@users.noreply.github.com>
2023-07-10 22:09:42 -0300

    config entitie games @Entity

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -1,9 +1,22 @@
 package com.devsuperior.dslist.entities;
 
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+@Entity
+@Table(name = "tb_game")
 public class Game {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id ;
 	private String title;
-	private int year;
+	@Column(name= "game_year")
+	private Integer year;
 	private String genre;
 	private String platform;
 	private String imgUrl;
@@ -88,6 +101,25 @@ public class Game {
 	public void setLongDescription(String longDescription) {
 		this.longDescription = longDescription;
 	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Game other = (Game) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
 	
 	
 	
